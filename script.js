5f6674066fae83fa8cecfd9823ac511f7207e70d
srijan <srijanprajapati2000@gmail.comm>
2026-05-10 21:43:32 +0530

    Added Google Sheets integration

--- script.js
+++ script.js
@@ -1,130 +1,8 @@
-// FAQ
-
-const questions = document.querySelectorAll(".faq-question");
-
-questions.forEach((question)=>{
-
-  question.addEventListener("click",()=>{
-
-    const answer = question.nextElementSibling;
-
-    if(answer.style.display === "block"){
-
-      answer.style.display = "none";
-
-    }
-
-    else{
-
-      answer.style.display = "block";
-
-    }
-
-  });
-
-});
-
-
-
-// TYPING EFFECT
-
-const words = [
-
-  "चैन छीन रही हैं!",
-  "नींद उड़ा रही हैं!",
-  "stress बढ़ा रही हैं!",
-  "health खराब कर रही हैं!"
-
-];
-
-let wordIndex = 0;
-let charIndex = 0;
-
-let currentWord = "";
-let currentChar = "";
-
-let isDeleting = false;
-
-const changingText = document.getElementById("changing-text");
-
-function type(){
-
-  currentWord = words[wordIndex];
-
-  if(isDeleting){
-
-    currentChar = currentWord.substring(0,charIndex--);
-
-  }
-
-  else{
-
-    currentChar = currentWord.substring(0,charIndex++);
-
-  }
-
-  changingText.textContent = currentChar;
-
-  let speed = 100;
-
-  if(isDeleting){
-
-    speed = 50;
-
-  }
-
-  if(!isDeleting && charIndex === currentWord.length){
-
-    speed = 1500;
-
-    isDeleting = true;
-
-  }
-
-  else if(isDeleting && charIndex === 0){
-
-    isDeleting = false;
-
-    wordIndex++;
-
-    if(wordIndex === words.length){
-
-      wordIndex = 0;
-
-    }
-
-  }
-
-  setTimeout(type,speed);
-
-}
-
-type();
-
-
-
-// PROBLEM SELECT
-
-const problemBoxes = document.querySelectorAll(".problem-box");
-
-problemBoxes.forEach((box)=>{
-
-  box.addEventListener("click",()=>{
-
-    box.classList.toggle("active");
-
-  });
-
-});
-
-
-
-
-// WHATSAPP FUNCTION
+// WHATSAPP + GOOGLE SHEETS FUNCTION
 
 const submitBtn = document.getElementById("submitBtn");
 
-submitBtn.addEventListener("click",()=>{
+submitBtn.addEventListener("click", async () => {
 
   const name =
   document.getElementById("name").value;
@@ -159,6 +37,27 @@ ${selectedProblems.join("\n")}
 अन्य जानकारी:
 ${message}`;
 
+
+  // GOOGLE SHEETS SAVE
+
+  await fetch("https://script.google.com/macros/s/AKfycbzZXp88iEfHq-JDfpZRBZCjnjxLjWHs1LxmBPrfgINwjMEPV0-mSXhhBRpRZvQVEYU1/exec", {
+
+    method: "POST",
+
+    body: JSON.stringify({
+
+      name: name,
+
+      phone: phone,
+
+      message: finalMessage
+
+    })
+
+  });
+
+
+  // WHATSAPP OPEN
 
   const whatsappNumber = "919580136415";
 
